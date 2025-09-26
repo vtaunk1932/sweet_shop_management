f631a695692b14f5e4995de223575f5959978bec
vasu <vasutaunk1932@gmail.com>
2025-09-26 10:34:57 +0530

    Added SweetshopApplication

---
+++ sweetshop/src/main/java/com/example/sweetshop/SweetshopApplication.java
@@ -0,0 +1,13 @@
+package com.example.sweetshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SweetshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SweetshopApplication.class, args);
+	}
+
+}

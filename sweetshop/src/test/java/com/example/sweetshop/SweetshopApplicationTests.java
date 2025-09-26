ea206646fe03810c602b180d40141848ebbbcf1b
vasu <vasutaunk1932@gmail.com>
2025-09-26 12:26:20 +0530

    Update application.properties

---
+++ sweetshop/src/test/java/com/example/sweetshop/SweetshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sweetshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SweetshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
